5c613cac826c74c01f6743613bcd014178742d68
abdo111000moxo-afk <abdo111000moxo@gmail.com>
2026-08-28 02:31:43 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8080;
+const MIME = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.glb': 'model/gltf-binary',
+  '.gltf': 'model/gltf+json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg'
+};
+
+http.createServer((req, res) => {
+  let reqPath = req.url.split('?')[0];
+  let file = path.join(__dirname, reqPath === '/' ? 'index.html' : reqPath);
+  let ext = path.extname(file);
+
+  fs.readFile(file, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+      res.end(content);
+    }
+  });
+}).listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
